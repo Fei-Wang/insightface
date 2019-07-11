5a3247a43b5c9b04625fb1f8d422b9c33654e453
Fei-Wang <fei.comm@icloud.com>
2019-07-11 21:29:12 +0800

    整理代码

--- recognition/backbones/ResNet_v1.py
+++ recognition/backbones/ResNet_v1.py
@@ -7,36 +7,6 @@ from logger import logger
 tf.enable_eager_execution()
 
 
-# class ResNetBlock_A(tf.keras.layers.Layer):
-#
-#     def __init__(self, filters=64, strides=(1, 1)):
-#         super(ResNetBlock_A, self).__init__()
-#         self.conv1 = tf.keras.layers.Conv2D(filters, (3, 3), padding='same', strides=strides)
-#         self.bn1 = tf.keras.layers.BatchNormalization()
-#         self.relu1 = tf.keras.layers.ReLU()
-#         self.conv2 = tf.keras.layers.Conv2D(filters, (3, 3), padding='same')
-#         self.bn2 = tf.keras.layers.BatchNormalization()
-#         self.relu2 = tf.keras.layers.ReLU()
-#         self.conv3 = tf.keras.layers.Conv2D(filters, (1, 1), padding='same', strides=strides)
-#         self.bn3 = tf.keras.layers.BatchNormalization()
-#
-#     def call(self, inputs):
-#         x = self.conv1(inputs)
-#         x = self.bn1(x)
-#         x = self.relu1(x)
-#         x = self.conv2(x)
-#         x = self.bn2(x)
-#         # print('A2', x.shape, inputs.shape, x.shape == inputs.shape)
-#         if x.shape == inputs.shape:
-#             res = inputs
-#         else:
-#             res = self.conv3(inputs)
-#             res = self.bn3(res)
-#         x += res
-#         x = self.relu2(x)
-#         return x
-
-
 class BasicBlock(tf.keras.layers.Layer):
 
     def __init__(self, filters=64, strides=(1, 1)):
@@ -55,7 +25,6 @@ class BasicBlock(tf.keras.layers.Layer):
         x = self.relu(x)
         x = self.conv2(x)
         x = self.bn2(x)
-        # print('A2', x.shape, inputs.shape, x.shape == inputs.shape)
         if x.shape == inputs.shape:
             res = inputs
         else:
@@ -64,42 +33,6 @@ class BasicBlock(tf.keras.layers.Layer):
         x += res
         x = self.relu(x)
         return x
-
-
-#
-# class ResNetBlock_B(tf.keras.layers.Layer):
-#
-#     def __init__(self, filters=64, strides=(1, 1)):
-#         super(ResNetBlock_B, self).__init__()
-#         self.conv1 = tf.keras.layers.Conv2D(filters, (1, 1), padding='same', strides=strides)
-#         self.bn1 = tf.keras.layers.BatchNormalization()
-#         self.relu1 = tf.keras.layers.ReLU()
-#         self.conv2 = tf.keras.layers.Conv2D(filters, (3, 3), padding='same')
-#         self.bn2 = tf.keras.layers.BatchNormalization()
-#         self.relu2 = tf.keras.layers.ReLU()
-#         self.conv3 = tf.keras.layers.Conv2D(filters * 4, (1, 1), padding='same')
-#         self.bn3 = tf.keras.layers.BatchNormalization()
-#         self.relu3 = tf.keras.layers.ReLU()
-#         self.conv4 = tf.keras.layers.Conv2D(filters * 4, (1, 1), padding='same', strides=strides)
-#         self.bn4 = tf.keras.layers.BatchNormalization()
-#
-#     def call(self, inputs):
-#         x = self.conv1(inputs)
-#         x = self.bn1(x)
-#         x = self.relu1(x)
-#         x = self.conv2(x)
-#         x = self.bn2(x)
-#         x = self.relu2(x)
-#         x = self.conv3(x)
-#         x = self.bn3(x)
-#         if x.shape == inputs.shape:
-#             res = inputs
-#         else:
-#             res = self.conv4(inputs)
-#             res = self.bn4(res)
-#         x += res
-#         x = self.relu3(x)
-#         return x
 
 
 class Bottleneck(tf.keras.layers.Layer):
@@ -135,101 +68,6 @@ class Bottleneck(tf.keras.layers.Layer):
         return x
 
 
-# class ResNetBlock_B2(tf.keras.layers.Layer):
-#
-#     def __init__(self, filters=64):
-#         super(ResNetBlock_B2, self).__init__()
-#         self.conv1 = tf.keras.layers.Conv2D(filters, (1, 1), padding='same', strides=(2, 2))
-#         self.bn1 = tf.keras.layers.BatchNormalization()
-#         self.relu1 = tf.keras.layers.ReLU()
-#         self.conv2 = tf.keras.layers.Conv2D(filters, (3, 3), padding='same')
-#         self.bn2 = tf.keras.layers.BatchNormalization()
-#         self.relu2 = tf.keras.layers.ReLU()
-#         self.conv3 = tf.keras.layers.Conv2D(filters * 4, (1, 1), padding='same')
-#         self.bn3 = tf.keras.layers.BatchNormalization()
-#         self.relu3 = tf.keras.layers.ReLU()
-#         self.conv4 = tf.keras.layers.Conv2D(filters * 4, (1, 1), padding='same', strides=(2, 2))
-#         self.bn4 = tf.keras.layers.BatchNormalization()
-#
-#     def call(self, inputs):
-#         x = self.conv1(inputs)
-#         x = self.bn1(x)
-#         x = self.relu1(x)
-#         x = self.conv2(x)
-#         x = self.bn2(x)
-#         x = self.relu2(x)
-#         x = self.conv3(x)
-#         x = self.bn3(x)
-#         print(x.shape, inputs.shape, x.shape == inputs.shape)
-#         res = self.conv4(inputs)
-#         res = self.bn4(res)
-#         x += res
-#         x = self.relu3(x)
-#         return x
-
-#
-# class ResNet_v1_18(tf.keras.Model):
-#     def __init__(self, num_classes=10):
-#         super(ResNet_v1_18, self).__init__()
-#         # self.num_classes = num_classes
-#         self.conv = tf.keras.layers.Conv2D(64, (7, 7), strides=(2, 2), padding='same')
-#         self.bn = tf.keras.layers.BatchNormalization()
-#         self.relu = tf.keras.layers.ReLU()
-#         self.maxpool = tf.keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')
-#         self.blocks1 = [ResNetBlock_A1(filters=64)] + [ResNetBlock_A1(filters=64) for _ in range(1)]
-#         self.blocks2 = [ResNetBlock_A2(filters=128)] + [ResNetBlock_A1(filters=128) for _ in range(1)]
-#         self.blocks3 = [ResNetBlock_A2(filters=256)] + [ResNetBlock_A1(filters=256) for _ in range(1)]
-#         self.blocks4 = [ResNetBlock_A2(filters=512)] + [ResNetBlock_A1(filters=512) for _ in range(1)]
-#         self.blocks = self.blocks1 + self.blocks2 + self.blocks3 + self.blocks4
-#         self.globalpool = tf.keras.layers.GlobalAveragePooling2D()
-#         self.dense = tf.keras.layers.Dense(num_classes)
-#         self.softmax = tf.keras.layers.Softmax()
-#
-#     def call(self, inputs, training=None, mask=None):
-#         x = self.conv(inputs)
-#         x = self.bn(x)
-#         x = self.relu(x)
-#         x = self.maxpool(x)
-#         for block in self.blocks:
-#             x = block(x)
-#         x = self.globalpool(x)
-#         x = self.dense(x)
-#         x = self.softmax(x)
-#
-#         return x
-#
-#
-# class ResNet_v1_34(tf.keras.Model):
-#     def __init__(self, num_classes=10):
-#         super(ResNet_v1_34, self).__init__()
-#         # self.num_classes = num_classes
-#         self.conv = tf.keras.layers.Conv2D(64, (7, 7), strides=(2, 2), padding='same')
-#         self.bn = tf.keras.layers.BatchNormalization()
-#         self.relu = tf.keras.layers.ReLU()
-#         self.maxpool = tf.keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')
-#         self.blocks1 = [ResNetBlock_A1(filters=64)] + [ResNetBlock_A1(filters=64) for _ in range(2)]
-#         self.blocks2 = [ResNetBlock_A2(filters=128)] + [ResNetBlock_A1(filters=128) for _ in range(3)]
-#         self.blocks3 = [ResNetBlock_A2(filters=256)] + [ResNetBlock_A1(filters=256) for _ in range(5)]
-#         self.blocks4 = [ResNetBlock_A2(filters=512)] + [ResNetBlock_A1(filters=512) for _ in range(2)]
-#         self.blocks = self.blocks1 + self.blocks2 + self.blocks3 + self.blocks4
-#         self.globalpool = tf.keras.layers.GlobalAveragePooling2D()
-#         self.dense = tf.keras.layers.Dense(num_classes)
-#         self.softmax = tf.keras.layers.Softmax()
-#
-#     def call(self, inputs, training=None, mask=None):
-#         x = self.conv(inputs)
-#         x = self.bn(x)
-#         x = self.relu(x)
-#         x = self.maxpool(x)
-#         for block in self.blocks:
-#             x = block(x)
-#         x = self.globalpool(x)
-#         x = self.dense(x)
-#         x = self.softmax(x)
-#
-#         return x
-
-
 class ResNet_v1(tf.keras.Model):
     def __init__(self, Block=Bottleneck, layers=(3, 4, 6, 3), num_classes=10):
         super(ResNet_v1, self).__init__()
@@ -253,7 +91,6 @@ class ResNet_v1(tf.keras.Model):
         x = self.maxpool(x)
         for block in self.blocks:
             x = block(x)
-        print(x.shape)
         x = self.globalpool(x)
         x = self.dense(x)
         x = self.softmax(x)
@@ -284,103 +121,6 @@ class ResNet_v1_101(ResNet_v1):
 class ResNet_v1_152(ResNet_v1):
     def __init__(self, num_classes=10):
         super(ResNet_v1_152, self).__init__(Block=Bottleneck, layers=(3, 8, 36, 3), num_classes=num_classes)
-
-
-#
-# class ResNet_v1_50(tf.keras.Model):
-#     def __init__(self, num_classes=10):
-#         super(ResNet_v1_50, self).__init__()
-#         # self.num_classes = num_classes
-#         self.conv = tf.keras.layers.Conv2D(64, (7, 7), strides=(2, 2), padding='same')
-#         self.bn = tf.keras.layers.BatchNormalization()
-#         self.relu = tf.keras.layers.ReLU()
-#         self.maxpool = tf.keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')
-#         self.blocks1 = [ResNetBlock_B(filters=64, strides=(1, 1)) for _ in range(3)]
-#         self.blocks2 = [ResNetBlock_B(filters=128, strides=(2, 2))] + [ResNetBlock_B(filters=128, strides=(1, 1)) for _
-#                                                                        in range(3)]
-#         self.blocks3 = [ResNetBlock_B(filters=256, strides=(2, 2))] + [ResNetBlock_B(filters=256, strides=(1, 1)) for _
-#                                                                        in range(5)]
-#         self.blocks4 = [ResNetBlock_B(filters=512, strides=(2, 2))] + [ResNetBlock_B(filters=512, strides=(1, 1)) for _
-#                                                                        in range(2)]
-#         self.blocks = self.blocks1 + self.blocks2 + self.blocks3 + self.blocks4
-#         self.globalpool = tf.keras.layers.GlobalAveragePooling2D()
-#         self.dense = tf.keras.layers.Dense(num_classes)
-#         self.softmax = tf.keras.layers.Softmax()
-#
-#     def call(self, inputs, training=None, mask=None):
-#         x = self.conv(inputs)
-#         x = self.bn(x)
-#         x = self.relu(x)
-#         x = self.maxpool(x)
-#         for block in self.blocks:
-#             x = block(x)
-#         x = self.globalpool(x)
-#         x = self.dense(x)
-#         x = self.softmax(x)
-#
-#         return x
-#
-#
-# class ResNet_v1_101(tf.keras.Model):
-#     def __init__(self, num_classes=10):
-#         super(ResNet_v1_101, self).__init__()
-#         # self.num_classes = num_classes
-#         self.conv = tf.keras.layers.Conv2D(64, (7, 7), strides=(2, 2), padding='same')
-#         self.bn = tf.keras.layers.BatchNormalization()
-#         self.relu = tf.keras.layers.ReLU()
-#         self.maxpool = tf.keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')
-#         self.blocks1 = [ResNetBlock_B1(filters=64)] + [ResNetBlock_B1(filters=64) for _ in range(2)]
-#         self.blocks2 = [ResNetBlock_B2(filters=128)] + [ResNetBlock_B1(filters=128) for _ in range(3)]
-#         self.blocks3 = [ResNetBlock_B2(filters=256)] + [ResNetBlock_B1(filters=256) for _ in range(22)]
-#         self.blocks4 = [ResNetBlock_B2(filters=512)] + [ResNetBlock_B1(filters=512) for _ in range(2)]
-#         self.blocks = self.blocks1 + self.blocks2 + self.blocks3 + self.blocks4
-#         self.globalpool = tf.keras.layers.GlobalAveragePooling2D()
-#         self.dense = tf.keras.layers.Dense(num_classes)
-#         self.softmax = tf.keras.layers.Softmax()
-#
-#     def call(self, inputs, training=None, mask=None):
-#         x = self.conv(inputs)
-#         x = self.bn(x)
-#         x = self.relu(x)
-#         x = self.maxpool(x)
-#         for block in self.blocks:
-#             x = block(x)
-#         x = self.globalpool(x)
-#         x = self.dense(x)
-#         x = self.softmax(x)
-#
-#         return x
-#
-#
-# class ResNet_v1_152(tf.keras.Model):
-#     def __init__(self, num_classes=10):
-#         super(ResNet_v1_152, self).__init__()
-#         # self.num_classes = num_classes
-#         self.conv = tf.keras.layers.Conv2D(64, (7, 7), strides=(2, 2), padding='same')
-#         self.bn = tf.keras.layers.BatchNormalization()
-#         self.relu = tf.keras.layers.ReLU()
-#         self.maxpool = tf.keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')
-#         self.blocks1 = [ResNetBlock_B1(filters=64)] + [ResNetBlock_B1(filters=64) for _ in range(2)]
-#         self.blocks2 = [ResNetBlock_B2(filters=128)] + [ResNetBlock_B1(filters=128) for _ in range(7)]
-#         self.blocks3 = [ResNetBlock_B2(filters=256)] + [ResNetBlock_B1(filters=256) for _ in range(35)]
-#         self.blocks4 = [ResNetBlock_B2(filters=512)] + [ResNetBlock_B1(filters=512) for _ in range(2)]
-#         self.blocks = self.blocks1 + self.blocks2 + self.blocks3 + self.blocks4
-#         self.globalpool = tf.keras.layers.GlobalAveragePooling2D()
-#         self.dense = tf.keras.layers.Dense(num_classes)
-#         self.softmax = tf.keras.layers.Softmax()
-#
-#     def call(self, inputs, training=None, mask=None):
-#         x = self.conv(inputs)
-#         x = self.bn(x)
-#         x = self.relu(x)
-#         x = self.maxpool(x)
-#         for block in self.blocks:
-#             x = block(x)
-#         x = self.globalpool(x)
-#         x = self.dense(x)
-#         x = self.softmax(x)
-#
-#         return x
 
 
 def parse_args(argv):
