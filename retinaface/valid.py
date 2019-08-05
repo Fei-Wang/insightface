ab65708059930c793a071eb213a54063d0ce621e
Fei-Wang <fei.comm@icloud.com>
2019-08-05 10:49:08 +0800

    add iou and lmk dist metric

--- retinaface/valid.py
+++ retinaface/valid.py
@@ -20,10 +20,21 @@ class ValidData:
         self.data = data
         self.au = au
 
+    @staticmethod
+    def _cal_dist(lmk1, lmk2):
+        dist = 0
+        square_value = (lmk2 - lmk1) ** 2
+        for i in range(lmk1.shape[0] // 2):
+            dist += np.sqrt(square_value[2 * i] + square_value[2 * i + 1])
+        dist /= lmk1.shape[0] / 2
+        return dist
+
     def get_metric(self, conf_thresh, iou_thresh, top_k):
         tps = 0
         fps = 0
         fns = 0
+        mean_iou = 0
+        mean_dist = 0
         for imgs, labels, _ in self.data:  # for each batch
             preds = predict(self.model, imgs, self.au)
             # print(preds.shape)
@@ -44,6 +55,9 @@ class ValidData:
                         iou = cal_iou(box[2:], label)
                         idx = np.argmax(iou)
                         if iou[idx] >= 0.5:
+                            if idx not in match:
+                                mean_iou += iou[idx]
+                                mean_dist += self._cal_dist(box[6:], label[idx, 4:])
                             match.add(idx)
                     tp = len(match)
                     fp = num_pred_truth - tp
@@ -55,14 +69,16 @@ class ValidData:
 
         p = tps / (tps + fps) if tps + fps != 0 else 0
         r = tps / (tps + fns) if tps + fns != 0 else 0
-        # print(p, r)
-        return p, r
+        mean_iou = mean_iou / tps if tps != 0 else 0
+        mean_dist = mean_dist / tps if tps != 0 else 0
+        # print(p, r, mean_iou, mean_dist)
+        return p, r, mean_iou, mean_dist
 
-    def draw_curve(self, iou_thresh, top_k):
+    def draw_curve(self, iou_thresh, top_k, num=100):
         P = []
         R = []
-        for thresh in np.linspace(0, 1, 100):
-            p, r = self.get_metric(thresh, iou_thresh, top_k)
+        for thresh in np.linspace(0, 1, num):
+            p, r, _, _ = self.get_metric(thresh, iou_thresh, top_k)
             P.append(p)
             R.append(r)
 
@@ -101,9 +117,9 @@ def main():
     # print("Restored from {}".format(tf.train.latest_checkpoint(ckpt_dir)))
 
     vd = ValidData(model, train_data, au)
-    p, r = vd.get_metric(0.6, 0.2, 100)
-    print(p, r)
-    vd.draw_curve(0.2, 100)
+    p, r, mean_iou, mean_dist = vd.get_metric(0.6, 0.2, 100)
+    print(p, r, mean_iou, mean_dist)
+    vd.draw_curve(0.2, 100, num=10)
 
 
 if __name__ == '__main__':
